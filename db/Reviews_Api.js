fee47de303a564bbfeeba5bcbe03819b374fd3a5
Unknown <bestbrett@aol.com>
2021-11-16 09:57:37 -1000

    Refactored methods to separate file from models

---
+++ db/Reviews_Api.js
@@ -0,0 +1,84 @@
+const Review = require('./Models/Review.js');
+const Characteristic = require('./Models/Characteristic.js');
+const Characteristic_Review = require('./Models/Characteristic_Review.js');
+const Reviews_Photo = require('./Models/Reviews_Photo.js');
+
+const getReviews = function(productId, page, count) {
+  let off = page * count - count;
+  return Review.findAll({ 
+    where: {
+      product_id: productId
+    },
+    offset: off, 
+    limit: count 
+  });
+}
+
+const getMeta = function(productId) {
+  let ratingsObj = {};
+  getRatings(productId).then((vals) => {
+    for (var i = 0; i < vals.length; i++) {
+      ratingsObj[i+1] = vals[i];
+    }
+  });
+  let recObj = {};
+  getRecommended(productId).then((val) => {
+    recObj[0] = val;
+  });
+}
+
+const getRatings = function(productId) {
+  let proms = [];
+  for (var i = 1; i < 6; i++) {
+    proms.push(Review.count({ 
+      where: {
+        product_id: productId,
+        rating: i,
+      },
+    }));
+  }
+  return Promise.all(proms);
+}
+const getRecommended = function(productId) {
+  return Review.count({ 
+    where: {
+      product_id: productId,
+      recommend: true,
+    },
+  });
+}
+const getChars = function(productId) {
+  return Characteristic.findAll({ 
+    where: {
+      product_id: productId
+    },
+  });
+}
+
+const getCharReviewAverage = function(charId) {
+  let proms = [Characteristic_Review.count({ 
+    where: {
+      characteristic_id: charId
+    },
+  }),
+  Characteristic_Review.sum('value', { 
+    where: {
+      characteristic_id: charId
+    },
+  })];
+  return Promise.all(proms).then((vals) => {
+    return vals[1]/vals[0];
+  });
+}
+
+
+const addReview = function(obj) {
+  obj.date = Date.now();
+  return Review.create(obj);
+}
+
+module.exports.getReviews = getReviews;
+module.exports.addReview = addReview;
+module.exports.getChars = getChars;
+module.exports.getCharReviewAverage = getCharReviewAverage;
+
--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const seq = require('../db/db.js');
-const Review = require('../db/Models/Review.js');
-const Characteristic = require('../db/Models/Characteristic.js');
-const Reviews_Photo = require('../db/Models/Reviews_Photo.js');
-const Characteristic_Review = require('../db/Models/Characteristic_Review.js');
+const Review_Api = require('../db/Reviews_Api.js');
 const app = express();
 const port = 3000;
 
-
+// Review.getChars(5).then((results) => {
+//   console.log(results);
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -17,3 +16,8 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
+// Review_Api.getCharReviewAverage(2).then((results) => {
+//   console.log(results);
+// }).catch((err) => {
+//   console.log(err);
+// });
