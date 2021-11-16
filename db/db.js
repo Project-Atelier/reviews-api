2c7b63990d3c5532c4f42042f76827e3392965b6
Unknown <bestbrett@aol.com>
2021-11-16 04:34:48 -1000

    sequelize initial setup

--- db/db.js
+++ db/db.js
@@ -1,7 +1,11 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
+const { user, database, password } = require('./config.js');
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(database, user, password, {
   dialect: 'postgres',
   host: '/var/run/postgresql'
 });
 
+sequelize.authenticate().catch((error) => console.log(error));
+
+module.export = sequelize;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sequelize = require('sequelize');
+const sequelize = require('../db/db.js');
 const app = express();
 const port = 3000;
 
